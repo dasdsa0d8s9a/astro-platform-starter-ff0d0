95a5e3c86bea9a46d032dbe06d1373ca09759ec5
bsjsjsbsnssu-glitch <bsjsjsbsnssu@gmail.com>
2026-01-18 00:59:37 +0100

    Update highlighter.ts

--- src/utils/highlighter.ts
+++ src/utils/highlighter.ts
@@ -4,3 +4,11 @@ export const highlighterPromise = createHighlighter({
     langs: ['jsx', 'js'],
     themes: ['min-dark']
 });
+exports.handler = async (event, context) => {
+  const apiKey = process.env.Hamid; // Accessing the secret
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Success" }),
+  };
+};
